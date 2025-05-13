0fe2cde73fc6a31b76936b86d3144bf7f3cc9750
Adrian Esdaile <adrian.esdaile@jpw.com.au>
2025-05-13 11:29:37 +1000

    i was totally going the wrong way

--- Program.cs
+++ Program.cs
@@ -4,14 +4,38 @@ using Casino;
 double odds = 0.75;
 Guy guy = new Guy() { Cash =100, Name = "The Player" };
 Console.WriteLine( "Welcome to the casino, " + guy.Name + "! /n The odds today are " + odds );
-while ( true )
+while ( guy.Cash > 0 )
 {
     guy.WriteMyInfo();
-    Console.WriteLine( "How much would you like to bet?" );
-    string howMuch = Console.ReadLine();
-    int amount = 
+    Console.WriteLine( "How much would you like to bet? (enter nothing to leave)" );
+    string? howMuch = Console.ReadLine();
+    if ( howMuch == "" ) return;
+    if ( int.TryParse(howMuch, out int amount ) )
+    {
+        int pot = guy.GiveCash( amount ) * 2;
+        if (  pot > 0 )
+        {
+            if (Random.Shared.NextDouble() > odds )
+            {
+                int winnings = pot;
+                Console.WriteLine( "You win!" );
+                guy.ReceiveCash( winnings );
+            }
+            else
+            {
+                Console.WriteLine( "You lose!" );
+            }
+        }
+    } 
+    else
+    { 
+        Console.WriteLine( "Please enter a valid amount." );
+    }
+
+
 
 }
+Console.WriteLine( "The house always wins!" );
 
 
 
