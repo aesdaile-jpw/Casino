9226e6d6ca75aba3a97aa729016b78080f4f5c7b
Adrian Esdaile <adrian.esdaile@jpw.com.au>
2025-05-13 10:47:37 +1000

    stuff

--- Guy.cs
+++ Guy.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Guys_and_Cash
+namespace Casino
 {
     internal class Guy
     {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Casino;
+
+double odds = 0.75;
+Guy guy = new Guy() { Cash =100, Name = "The Player" };
+Console.WriteLine( "Welcome to the casino, " + guy.Name + "! /n The odds today are " + odds );
+while ( true )
+{
+    guy.WriteMyInfo();
+    Console.WriteLine( "How much would you like to bet?" );
+    string howMuch = Console.ReadLine();
+    int amount = 
+
+}
+
 
 
